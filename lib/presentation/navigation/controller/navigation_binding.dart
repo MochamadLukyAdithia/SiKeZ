26b52a326b791aa5ee5f253e47172228b98cb2cb
Reihan Wudd Hibatullah <116327302+ninkyuwudd@users.noreply.github.com>
2024-09-07 19:05:16 +0700

    update dashboard

--- lib/presentation/navigation/controller/navigation_binding.dart
+++ lib/presentation/navigation/controller/navigation_binding.dart
@@ -2,7 +2,13 @@ import 'package:get/get.dart';
 import 'package:hmj_apps/core/injector/injector.dart';
 import 'package:hmj_apps/presentation/dasboard/controller/dashboard_controller.dart';
 import 'package:hmj_apps/presentation/navigation/controller/navigation_controller.dart';
+import 'package:hmj_apps/presentation/report/controller/report_buku_controller.dart';
 import 'package:hmj_apps/presentation/report/controller/report_controller.dart';
+import 'package:hmj_apps/presentation/report/controller/report_jurnal_controller.dart';
+import 'package:hmj_apps/presentation/report/controller/report_laba_controller.dart';
+import 'package:hmj_apps/presentation/report/controller/report_modal_controller.dart';
+import 'package:hmj_apps/presentation/report/controller/report_neraca_controller.dart';
+import 'package:hmj_apps/presentation/report/controller/report_neraca_saldo_controller.dart';
 
 class NavigationBinding extends Bindings {
   @override
@@ -10,5 +16,11 @@ class NavigationBinding extends Bindings {
     Get.put(getIt<NavigationController>());
     Get.put(getIt<ReportController>());
     Get.put(DashboardController());
+    // Get.lazyPut(() => ReportJurnalController());
+    // Get.lazyPut(() => BukuBesarController());
+    // Get.lazyPut(() => NeracaSaldoController());
+    // Get.lazyPut(() => ReportLabaRugiController());
+    // Get.lazyPut(() => ReportModalController());
+    // Get.lazyPut(() => ReportNeracaController());
   }
 }
